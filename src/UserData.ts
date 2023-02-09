aa563486d72ea13ae716897680cec94997bdbbee
Zuber Marcell <zubermarcellpetrik@gmail.com>
2023-02-09 16:29:50 +0100

    added registration date email, birthdate

--- src/UserData.ts
+++ src/UserData.ts
@@ -1,4 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, IsString, MinLength,IsDate,IsOptional } from 'class-validator';
+import { Column, Entity, IsNull, PrimaryGeneratedColumn } from 'typeorm';
+
+const now = new Date();
+function padTo2Digits(num: number) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date: Date) {
+  return (
+    [
+      date.getFullYear(),
+      padTo2Digits(date.getMonth() + 1),
+      padTo2Digits(date.getDate()),
+    ].join('-')) +
+    ' ' +
+    [
+      padTo2Digits(date.getHours()),
+      padTo2Digits(date.getMinutes()),
+      padTo2Digits(date.getSeconds()),
+    ].join(':');
+  }
 
 @Entity()
 export default class UserData {
@@ -8,12 +29,26 @@ export default class UserData {
   @Column()
   username: string;
 
+  @IsString()
+  @MinLength(6)
   @Column()
   password: string;
 
+  @IsString()
+  @MinLength(6)
   @Column()
   passwordAuth: string; //the password again to make sure it matches.
 
+  @IsEmail()
   @Column()
   email: string;
+
+  @IsDate()
+  @Column({default: formatDate(now)})
+  birthDate: Date;
+
+  @IsDate()
+  @IsOptional()
+  @Column({default: formatDate(now)})
+  registrationDate: Date;
 }
