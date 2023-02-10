e6c80195f9dd74bb83bea1e00a8104d7ff578188
Zuber Marcell <zubermarcellpetrik@gmail.com>
2023-02-10 08:50:55 +0100

    updated userData import name

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import UserData from './UserData';
+import UserData from './UserData.dto';
 
 @Module({
   imports: [
