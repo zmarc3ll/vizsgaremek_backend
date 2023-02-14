f6255e280372c5a4f9340b31bec2fed409eeab80
LeviVitez <bmwyolo555@gmail.com>
2023-02-14 14:00:01 +0100

    id, number, model hozzáadása

--- src/CarData.dto.ts
+++ src/CarData.dto.ts
@@ -1,5 +1,15 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import UserData from './UserData.dto';
 
 @Entity()
-export default class CarData {}
+export default class CarData {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  model: string;
+
+}
