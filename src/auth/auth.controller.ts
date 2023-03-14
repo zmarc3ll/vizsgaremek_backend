28b670160aa5b2d3fade06558f5eb3deed04ac78
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-14 09:19:06 +0100

    auth controller done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AppService } from 'src/app.service';
+import UserData from 'src/UserData.entity';
+import { DataSource } from 'typeorm';
+import loginDto from './login.dto';
+import * as bcrypt from 'bcrypt';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+constructor(private dataSource: DataSource, private authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() loginData: loginDto) {
+            const userRepo = this.dataSource.getRepository(UserData);
+            const user = await userRepo.findOneBy({ username: loginData.username });
+            if(user === null) {
+                throw new UnauthorizedException(' Hibás felhasználónév vagy jelszó!');
+            }
+            if (!await bcrypt.compare(loginData.password, user.password)) {
+                throw new UnauthorizedException('Hibás felhasználónév vagy jelszó!');                
+            }
+
+        return {
+            token: await this.authService.generateTokenFor(user),
+        };
+    }
+}
